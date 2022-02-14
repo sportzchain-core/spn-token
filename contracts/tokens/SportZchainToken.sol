65d862bf66958becad0cb08bce460aa413e43101
atptaxa <aarasu@atpco.net>
2022-02-14 01:47:36 -0500

    updated contract

--- contracts/tokens/SportZchainToken.sol
+++ contracts/tokens/SportZchainToken.sol
@@ -5,63 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @dev An ERC20 implementation of the SportZchain ecosystem token. All tokens are initially pre-assigned to
  * the creator, and can later be distributed freely using transfer transferFrom and other ERC20 functions.
  */
-contract SportZchainToken is Ownable, ERC20Votes, ERC20Pausable, ERC20Burnable {
-
-    // SportZchain token upper limit
-    uint256 supplyUpperLimit;
-
-    // decimal value of the token
-    uint8 decimalValue;
-
+contract SportZchainToken is Ownable, ERC20Pausable, ERC20Burnable, AccessControl {
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      *
      * @param _name = token name
      * @param _symbol = token symbol
      * @param _decimals = token decimals
-     * @param _supplyUpperLimit = token supply upper limit.
-     * @param _initialSupply = token amount to increase
+     * @param _totalSupply = token supply of the token
      */
     constructor (
         string memory _name,
         string memory _symbol,
         uint8 _decimals,
-        uint256 _supplyUpperLimit,
-        uint256 _initialSupply
-    ) ERC20(_name, _symbol) ERC20Permit(_name) {
-        decimalValue = _decimals;
-        uint256 _initSupply = _initialSupply * (10 ** uint256(_decimals));
-
-        // Token upper limit is fixed during the creation of the contract.
-        supplyUpperLimit = _supplyUpperLimit * (10 ** uint256(_decimals));
-
+        uint256 _totalSupply
+    ) ERC20(_name, _symbol)  {
         // This is the only place where we ever mint tokens for initial supply
-        _mint(msg.sender, _initSupply);
-    }
-
-
-    /**
-     * @dev Allow only the owner of the contract to increase the supply of the token.
-     * The max supply is restricted by the token upper limit.
-     *
-     * @param amount = token amount to increase
-     */
-    function mint(
-        uint256 amount
-    )
-    external
-    onlyOwner {
-        uint256 _amount = amount * (10 ** uint256(decimalValue));
-        uint256 _finalSupply = totalSupply() + _amount;
-        require(_finalSupply <= supplyUpperLimit, "Cant mint more than supply limit ");
-        _mint(msg.sender, _amount);
+        _mint(msg.sender,  _totalSupply * (10 ** uint256(_decimals)));
     }
 
 
@@ -70,60 +37,20 @@ contract SportZchainToken is Ownable, ERC20Votes, ERC20Pausable, ERC20Burnable {
      * supply. There is no reason for a token holder to EVER call this method directly. It will be
      * used by the future SportZchain ecosystem token contract to implement token redemption.
      *
-     * @param amount = token amount to burn
+     * @param from = burn from account
+     * @param _amount = token amount to burn
      */
     function burn(
-        uint256 amount
+        address from,
+        uint256 _amount
     )
     public
-    override (ERC20Burnable)
-    onlyOwner {
-        uint256 _amount = amount * (10 ** uint256(decimalValue));
+    override (ERC20Burnable) {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
         // This is the only place where we ever burn tokens.
-        _burn(msg.sender, _amount);
-        supplyUpperLimit -= _amount;
+        _burn(from, _amount);
     }
 
-
-    /**
-     * @dev Overrides _mint function - See ERC20 and ERC20Votes
-     *
-     * @param to = address to which the tokens needs to be minted
-     * @param amount = token amount to mint
-     */
-    function _mint(address to, uint256 amount)
-    internal
-    override(ERC20, ERC20Votes)
-    {
-        super._mint(to, amount);
-    }
-
-    /**
-     * @dev Overrides _burn function - See ERC20 and ERC20Votes
-     *
-     * @param account = address from which the tokens needs to be burned
-     * @param amount = token amount to burn
-     */
-    function _burn(address account, uint256 amount)
-    internal
-    override(ERC20, ERC20Votes)
-    {
-        super._burn(account, amount);
-    }
-
-    /**
-     * @dev Overrides _afterTokenTransfer - See ERC20 and ERC20Votes
-     *
-     * @param from = address from which the tokens needs to be transferred
-     * @param to = address to  which the tokens needs to be transferred
-     * @param amount = token amount to be transferred
-     */
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-    internal
-    override(ERC20, ERC20Votes)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
 
     /**
      * @dev Overrides _afterTokenTransfer - See ERC20 and ERC20Pausable
